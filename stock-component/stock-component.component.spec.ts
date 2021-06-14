7bed0ca49b3058067a850dca7078034132b964ef
vineeshayarlagadda <75362532+vineeshayarlagadda@users.noreply.github.com>
2021-06-15 00:55:32 +0530

    Add files via upload

---
+++ stock-component/stock-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StockComponentComponent } from './stock-component.component';
+
+describe('StockComponentComponent', () => {
+  let component: StockComponentComponent;
+  let fixture: ComponentFixture<StockComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StockComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StockComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
